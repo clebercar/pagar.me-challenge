dde581aac4f1afe0db346cc3703dcac78ea5f3f1
clebercar <contatoclebercarvalho@gmail.com>
2019-06-30 22:50:29 -0300

    Ajusta a configuração das migrations user e transactions

---
+++ app/src/database/migrations/20190630032414-create-transactions.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('transactions', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      payment_type: {
+        type: Sequelize.ENUM('debit_card', 'credit_card'),
+        allowNull: false,
+      },
+      card_number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      name_on_card: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      expiration_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      cvv: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('transactions')
+  }
+}

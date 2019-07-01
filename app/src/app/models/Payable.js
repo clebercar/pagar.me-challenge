cdeb33e2fc4e4fcd63310b84e4628d642bbf3b08
clebercar <contatoclebercarvalho@gmail.com>
2019-06-30 22:49:16 -0300

    Configuração inicial da model Payable

---
+++ app/src/app/models/Payable.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Payable = sequelize.define('Payable',{
+    status: DataTypes.ENUM('paid', 'waiting_funds'),
+    fee: DataTypes.DECIMAL(10,2),
+  })
+
+  Payable.associate = models => {
+    Payable.hasOne(models.Transaction, {foreignKey: 'transaction_id', as: 'transaction'});
+  }
+
+  return Payable
+}

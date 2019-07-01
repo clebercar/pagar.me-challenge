b1198876532b99c4b5ec043736a240371b1cb6d0
clebercar <contatoclebercarvalho@gmail.com>
2019-06-30 22:49:35 -0300

    Configuração inicial da model Transaction

---
+++ app/src/app/models/Transaction.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Transaction = sequelize.define('Transaction',{
+    value: DataTypes.DECIMAL(10,2),
+    description: DataTypes.TEXT,
+    payment_type: DataTypes.ENUM('debit_card', 'credit_card'),
+    card_number: DataTypes.INTEGER,
+    name_on_card: DataTypes.STRING,
+    expiration_date: DataTypes.DATE,
+    cvv: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  })
+
+  Transaction.associate = models => {
+    Transaction.belongsTo(models.User, {foreignKey: 'user_id', as: 'user'})
+  }
+
+  return Transaction
+}

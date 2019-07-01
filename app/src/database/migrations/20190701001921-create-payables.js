c5240e97d611a53827f8c86658c0dcab770660b5
clebercar <contatoclebercarvalho@gmail.com>
2019-06-30 22:50:58 -0300

    Configuração da migration de payables

---
+++ app/src/database/migrations/20190701001921-create-payables.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('payables', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM('paid', 'waiting_funds'),
+        allowNull: false,
+      },
+      fee: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false,
+      },
+      transaction_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: 'transactions',
+          key: 'id'
+        }
+      },
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('payables')
+  }
+}

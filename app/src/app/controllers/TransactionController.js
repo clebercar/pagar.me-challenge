03f8f18f6f94efc1ea7bb84c1ec28d0920847454
clebercar <contatoclebercarvalho@gmail.com>
2019-07-03 01:31:39 -0300

    Imepleta processo tempoario de criação de transaction e payables

--- app/src/app/controllers/TransactionController.js
+++ app/src/app/controllers/TransactionController.js
@@ -1,4 +1,5 @@
 const TransactionRepository = require('../repositories/TransactionRepository')
+const { Payable } = require('../models')
 
 class TransactionController {
   async index (req, res) {
@@ -8,6 +9,14 @@ class TransactionController {
 
   async create (req, res) {
     const transaction = await TransactionRepository.create(req.body)
+
+    await Payable.create({
+      status: 'paid',
+      payment_date: '2020-12-15',
+      amount: 320.50,
+      transaction_id: transaction.id
+    })
+
     res.json(transaction)
   }
 }

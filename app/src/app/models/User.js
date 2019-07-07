1dc64a0f07c7368e267d1bccf7cee2f23b6d2a98
clebercar <contatoclebercarvalho@gmail.com>
2019-07-07 03:08:51 -0300

    Adiciona validações de campo na model user

--- app/src/app/models/User.js
+++ app/src/app/models/User.js
@@ -1,8 +1,39 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [4, 20],
+          msg: 'This field Name must be between 4 and 20 characters.'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'This field Email must be an email valid.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.VIRTUAL,
+      len: {
+        args: [6, 8],
+        msg: 'This field Password must be between 6 and 8 characters.'
+      }
+    },
+    password_hash: DataTypes.STRING
+  },
+  {
+    hooks: {
+      beforeSave: async user => {
+        if (user.password) {
+          user.password_hash = await bcrypt.hash(user.password, 8)
+        }
+      }
+    }
   })
 
   return User

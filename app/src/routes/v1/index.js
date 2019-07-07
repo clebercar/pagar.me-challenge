116b62f179c4ed48fc5be91d84f30b26f282c09d
clebercar <contatoclebercarvalho@gmail.com>
2019-07-07 03:14:30 -0300

    Adiciona novas rotas

--- app/src/routes/v1/index.js
+++ app/src/routes/v1/index.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
+const authMiddleware = require('../../app/middlewares/auth')
 const TransactionsRoutes = require('./TransactionsRoutes')
 const PayablesRoutes = require('./PayablesRoutes')
 const UsersRoutes = require('./UsersRoutes')
+const SessionRoutes = require('./SessionRoutes')
+
+router.use('/v1', UsersRoutes)
+router.use('/v1', SessionRoutes)
+
+router.use(authMiddleware)
 
 router.use('/v1', TransactionsRoutes)
 router.use('/v1', PayablesRoutes)
-router.use('/v1', UsersRoutes)
 
 module.exports = router

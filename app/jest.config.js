cdc92d6485660a9234ca019463456a9688024538
clebercar <contatoclebercarvalho@gmail.com>
2019-07-07 23:23:51 -0300

    Ajusta a suíte de testes

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   bail: true,
-  clearMocks: true,
-  coverageDirectory: "__tests__/coverage",
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/__tests__/'],
-  testEnvironment: "node",
-  testMatch: [
-    "**/__tests__/**/*.test.js?(x)",
+  verbose: true,
+  setupFilesAfterEnv: [
+    '<rootDir>/__tests__/setup.js'
   ],
+  clearMocks: true,
+  coverageDirectory: '__tests__/coverage',
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/__tests__/'],
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.js?(x)'
+  ]
 }

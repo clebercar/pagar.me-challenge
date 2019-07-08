eefd29f9a0be8ba758977da3948c1998eaa24461
clebercar <contatoclebercarvalho@gmail.com>
2019-07-07 23:27:02 -0300

    Teste de integração do User Controller

---
+++ app/__tests__/integration/controllers/UserController.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const app = require('../../../src/app')
+const factory = require('../../factories')
+
+describe('User Controller', () => {
+  describe('POST Create', () => {
+    let userAttrs
+    let response
+
+    beforeEach(async () => {
+      userAttrs = await factory.attrs('User')
+        .then(attributes => attributes)
+
+      response = await request(app)
+        .post('/api/v1/users')
+        .send(userAttrs)
+    })
+
+    it('should return an users created', async () => {
+      expect(typeof response.body).toBe('object')
+    })
+
+    it('should return status 200', () => {
+      expect(response.status).toBe(200)
+    })
+  })
+})
